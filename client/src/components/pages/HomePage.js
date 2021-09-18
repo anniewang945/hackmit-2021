fd49f6170bb05aa123b04618a29e164c17da1cbc
Feina Niu <feinaniu@mit.edu>
2021-09-18 13:30:53 -0400

    Add Google OAuth to HomePage

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import NavBar from "../modules/NavBar.js";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 import "../../utilities.css";
 import "./Skeleton.css";
+
+const GOOGLE_CLIENT_ID = "768117289293-7f5hjm2agb39e37sdc0srlu73i9tnq0e.apps.googleusercontent.com";
 
 class HomePage extends Component {
   constructor(props) {
@@ -20,6 +23,23 @@ class HomePage extends Component {
           <NavBar />
         </section>
         <div>Lots to do here!</div>
+        <div>
+        {this.props.userId ? (
+          <GoogleLogout
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={this.props.handleLogout}
+            onFailure={(err) => console.log(err)}
+          />
+        ) : (
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.props.handleLogin}
+            onFailure={(err) => console.log(err)}
+          />
+        )}
+        </div>
       </>
     );
   }
